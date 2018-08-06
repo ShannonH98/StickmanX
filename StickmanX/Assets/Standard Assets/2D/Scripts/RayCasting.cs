091ee1a47f26f048da550f030a0a22257847b2df
Lychaon <wembleywilliams@gmail.com>
2018-08-06 12:15:47 -0500

    Added new test map and stock assets along with a patrolling enemy that follows the player once player enters a certain radius

---
+++ StickmanX/Assets/Standard Assets/2D/Scripts/RayCasting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour {
+
+    public Transform sightStart, sightEnd;
+	// Update is called once per frame
+	void Update () {
+        Raycasting();
+        Behaviours();
+	}
+
+    void Raycasting()
+    {
+        Debug.DrawLine(sightStart.position, sightEnd.position, Color.red);
+    }
+    
+    void Behaviours()
+    {
+
+    }
+}
